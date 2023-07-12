89d53dc63da43b00ef1c61e20e65a9085b5f3572
pedroberri <110639002+pedroberri@users.noreply.github.com>
2023-07-12 16:12:34 -0300

    Update V1__add_fulltext.sql

--- src/main/resources/db/migration/V1__add_fulltext.sql
+++ src/main/resources/db/migration/V1__add_fulltext.sql
@@ -1,3 +1,3 @@
 ALTER TABLE video ADD FULLTEXT INDEX video_search (titulo);
 ALTER TABLE tag ADD FULLTEXT INDEX tag_search (tag);
-ALTER TABLE categoria ADD FULLTEXT INDEX categoria_search (categoria);
+ALTER TABLE categoria ADD FULLTEXT INDEX categoria_search (categoriaString);
